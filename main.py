bd838357854a7c237c440bf37cc921cb7ec127c9
Jun Lee <82641992+junlee-3@users.noreply.github.com>
2022-09-01 10:10:00 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def is_even(number):
+    return number % 2 == 0
+
+def get_even_letters(message):
+    even_letters = []
+    for counter in range(0, len(message)):
+        if is_even(counter):
+            even_letters.append(message[counter])
+    return even_letters
